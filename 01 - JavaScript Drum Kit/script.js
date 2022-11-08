981d81516b17cc96eda3fccfe971374aecfd859f
Thunderblarg <Thunderblarg@gmail.com>
2022-11-07 18:37:31 -0800

    Finished with drum kit project as per TOP assignment

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,14 +1,47 @@
-window.addEventListener('keydown', e => {
-    console.log("we're in the event listener");
-    const audio = document.querySelector(`audio[data-key="${e.Code}]`);
-    const key = document.querySelector(`.key[data-key="${e.Code}"]`);
-    if (!audio) return;
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add('playing')
+ready(()=>{
+    window.addEventListener('keydown', e => {
+        console.log("blerp");
+        const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+        const key = document.querySelector(`.key[data-key="${e.code}"]`);
+        if (!audio) return;
+        audio.currentTime = 0;
+        audio.play();
+        key.classList.add('playing');
+    });
+
+    const keys = document.querySelectorAll('.key');
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 });
+
+function playSound(e){
+
+}
 
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     this.classList.remove('playing');
 }
+
+
+function ready(fn) {
+    if (document.readyState !== 'loading') {
+      fn();
+    } else {
+      document.addEventListener('DOMContentLoaded', fn);
+    }
+  }
+
+//document.addEventListener(DOMContentLoaded, () => {
+    
+//});
+
+
+    //{
+    //console.log("we're in the event listener");
+    /*
+
+    */
+//});
+
+//function foo(e){};
+
